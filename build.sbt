2138951f903a07ad8cd427b294f665e3fca380ee
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2022-08-17 04:12:44 +0700

    Executor.currentThread()

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ scalaVersion := "2.13.8"
 
 scalacOptions ++= Seq(
     "-encoding", "utf8",
-//    "-Xfatal-warnings", todo uncomment
+    "-Xfatal-warnings",
     "-Xlint",
     "-target:jvm-17",
 )
--- src/main/scala/com/github/skozlov/async/Executor.scala
+++ src/main/scala/com/github/skozlov/async/Executor.scala
@@ -4,5 +4,11 @@ import scala.concurrent.duration.Duration
 import scala.util.Try
 
 trait Executor {
-    def execute[R](f: () => R)(implicit timeout: Duration): Option[Try[R]]
+    def execute[R](r: => R)(implicit blockingTimeout: Duration): Option[Try[R]]
+}
+
+object Executor {
+    def currentThread(): Executor = new Executor {
+        override def execute[R](r: => R)(implicit blockingTimeout: Duration): Option[Try[R]] = Some(Try{r})
+    }
 }
