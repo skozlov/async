dc307ca0d44a5cb0ece1ebdb959aab615c54a3d9
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2022-08-19 13:48:45 +0700

    Improved AsyncTask

--- src/main/scala/com/github/skozlov/async/AsyncTask.scala
+++ src/main/scala/com/github/skozlov/async/AsyncTask.scala
@@ -1,11 +1,18 @@
 package com.github.skozlov.async
 
 trait AsyncTask{
+    def checkStillNeeded(): Boolean = !checkDeadlineOver()
+
     def perform(): Unit
 
     def deadline: Deadline
 
     def onDeadlineOver(): Unit
 
-    def checkStillNeeded(): Boolean
+    def checkDeadlineOver(): Boolean = {
+        if (deadline.isOver) {
+            onDeadlineOver()
+            true
+        } else false
+    }
 }
