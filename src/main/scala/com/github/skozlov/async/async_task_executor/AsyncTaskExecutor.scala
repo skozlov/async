0e69f2781b8b7fa185f5ad8ecb8fa884e16ea829
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2022-08-22 00:53:04 +0700

    Removed AsyncTaskExecutor.Immediate as using the current thread to execute tasks can lead to deadlock

--- src/main/scala/com/github/skozlov/async/async_task_executor/AsyncTaskExecutor.scala
+++ src/main/scala/com/github/skozlov/async/async_task_executor/AsyncTaskExecutor.scala
@@ -3,7 +3,3 @@ package com.github.skozlov.async.async_task_executor
 trait AsyncTaskExecutor{
     def submit(task: AsyncTask): Unit
 }
-
-object AsyncTaskExecutor{
-    val Immediate: AsyncTaskExecutor = (task: AsyncTask) => if (task.checkStillNeeded()) task.perform()
-}
